3b4820a1c13cdc3c6b77a109b7fedec2955ac7e7
Olga Pudrovska <opudrovs@gmail.com>
2019-01-23 01:19:19 +0200

    Added D3.js library and type definitions for D3.js.
    Added template for D3 bar chart component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import BarChart from './components/charts/BarChart/BarChart';
+
+import './styles/App.css';
+
+const data = require('./data/BarChart.json');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <header className="App-header"/>
+          <div className="barChart">
+              <BarChart
+                  data={data}
+                  style={{ backgroundColor: '#ccc' }}
+              />
+          </div>
       </div>
     );
   }
---
+++ src/components/charts/BarChart/BarChart.tsx
@@ -0,0 +1,35 @@
+import React, { Component, CSSProperties } from 'react';
+
+import {select} from 'd3-selection';
+
+import './BarChart.css';
+
+interface Props {
+    data: number[];
+    style: CSSProperties;
+}
+
+export default class BarChart extends Component<Props> {
+    private svgRef?: SVGElement | null;
+
+    public componentDidMount() {
+        this.drawChart(this.props.data);
+    }
+
+    private drawChart(data: number[]) {
+        const svg = select(this.svgRef!);
+        // Draw chart here.
+    }
+
+    public render() {
+        const { style } = this.props;
+
+        return (
+            <svg
+                className="barChart"
+                style={style}
+                ref={ref => (this.svgRef = ref)}
+            />
+        );
+    }
+}
