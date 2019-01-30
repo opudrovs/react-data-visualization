8bfa89f498327cde33dcfe0c118b5111347e296b
Olga Pudrovska <opudrovs@gmail.com>
2019-01-30 21:55:22 +0200

    Updates

---
+++ precache-manifest.c94901b992060b5dd2701ad3db398fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "966cc4973a813c8c3875",
+    "url": "/static/css/main.d48ad5bb.chunk.css"
+  },
+  {
+    "revision": "966cc4973a813c8c3875",
+    "url": "/static/js/main.966cc497.chunk.js"
+  },
+  {
+    "revision": "6d3b495548b380f8438c",
+    "url": "/static/js/1.6d3b4955.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a32c450a1664cd59797ed7c48f84fcef",
+    "url": "/index.html"
+  }
+];
